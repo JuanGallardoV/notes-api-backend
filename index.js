57be7d89325eaf8b748a0f895bf529c03a4f864c
Juan Gallardo <juangallardovergara17@gmail.com>
2022-03-21 17:02:48 -0300

    Config de Heroku

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.use((request, response) => {
 })
 
 //* Ya que el levantamiento tarda un poco, con este codigo una vez terminado de levantar el servidor, imprime por consola el msg
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
